f6d02ef34bea2eac596be05bed50aee1a5c50ef2
Rich Siegel <rsiegel@ise.com>
2016-06-25 00:44:33 -0400

    arithmetic and first function

---
+++ 1.ml
@@ -0,0 +1,22 @@
+(* here we go *)
+8 / 3
+(* result * - : int = 2 *)
+3.5 +. 6.
+(* - : float = 9.5 *)
+30_000_000 / 300_000
+(* - : int = 100 *)
+sqrt 9.
+(* - : float = 3. *)
+
+(* summary note - underscores used in numbers - dot to signal float *)
+(* use trailing ;; in top level *)
+
+# let x7 = 3 + 4
+(* use let for variable assignment - must start lowercase and w/ underscore *)
+
+(* example function *)
+
+let square x = x * x
+(* then to call func do *)
+square 2 (* - : int = 4 *)
+
